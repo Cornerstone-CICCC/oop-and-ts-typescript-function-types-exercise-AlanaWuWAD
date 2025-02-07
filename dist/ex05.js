defb37868057b73457571ae6233cc60c6c94f3c7
Alana <rcclewu@gmail.com>
2025-02-06 23:12:29 -0800

    done

---
+++ dist/ex05.js
@@ -0,0 +1,12 @@
+"use strict";
+// Create a function called sumAllNumbers.
+// This function should accept any number of numbers using a rest parameter.
+// It should return the sum of all the numbers passed to it.
+function sumAllNumbers(...num) {
+    let sum = 0;
+    for (let i = 0; i < num.length; i++) {
+        sum += num[i];
+    }
+    return sum;
+}
+console.log(sumAllNumbers(1, 2, 3, 4, 5)); // Expected output: 15
--- src/ex05.ts
+++ src/ex05.ts
@@ -2,8 +2,12 @@
 // This function should accept any number of numbers using a rest parameter.
 // It should return the sum of all the numbers passed to it.
 
-function sumAllNumbers( ) {
-
+function sumAllNumbers(...num: number[] ):number {
+    let sum = 0
+    for(let i=0; i< num.length; i++){
+        sum += num[i]
+    }
+    return sum
 }
 
 console.log(sumAllNumbers(1, 2, 3, 4, 5)); // Expected output: 15
